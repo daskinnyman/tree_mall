8c1e4765dcbfb97dbe0d2259327a745fb66a738c
陳寶桁 <>
2021-03-27 22:20:56 +0800

    create types and interfaces for orders

---
+++ src/interfaces/order.ts
@@ -0,0 +1,13 @@
+type StatusType = "進行中" | "已完成 ";
+
+interface OrderStatus {
+  code: number;
+  type: StatusType;
+}
+
+export interface Order {
+  name: string;
+  logo: string;
+  status: OrderStatus;
+  date: string;
+}
